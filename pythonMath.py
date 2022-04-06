d5cc05c7812ac4dfe903a520e96e6a542606f55b
gabe.certs <gabe.certs@gmail.com>
2022-04-06 17:55:41 -0500

    testing branch

--- pythonMath.py
+++ pythonMath.py
@@ -2,4 +2,4 @@ print(25 * 25)
 
 #Concatenation
 print("Hello Contatenation " + str(50) + " World")
-print(f"Hello Concatenation {50 + 10 * 2} world") #using elegant way with f from format 
+print(f"Hello Concatenation {50 + 10 * 2} world") #using elegant way with f from format d
